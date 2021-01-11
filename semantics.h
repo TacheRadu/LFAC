20e38db63d6b115023ad1363fc3244ae140c2f47
Radu Ioan Tache <radutache76@gmail.com>
2021-01-11 09:43:27 +0200

    These should be here

--- semantics.h
+++ semantics.h
@@ -1,6 +1,5 @@
 #include "scope.h"
 
-#define max(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 void set_tip(struct scope_entry* e, char* tip){
     struct scope_entry* it = e;
@@ -119,67 +118,4 @@ void display(struct scope *s){
         }
         it = it->next;
     }
-}
-
-struct expr_type* create_expr(float n){
-    struct expr_type *res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = 3;
-    res->val = n;
-    return res;
-}
-
-struct expr_type* create_expr(int n){
-    struct expr_type *res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = 0;
-    res->val = (float)n;
-    return res;
-}
-
-struct expr_type* create_expr(char c){
-    struct expr_type *res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = 1;
-    res->val = (float)c;
-    return res;
-}
-
-struct expr_type* create_expr(int b, int smth){
-    struct expr_type *res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = 2;
-    res->val = (float)b;
-    return res;
-}
-
-struct expr_type* logical_operations(struct expr_type *left, struct expr_type *right, char *op){
-    struct expr_type *res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = 2;
-    if(strcmp(op, "==") == 0)
-        res->val = (float)(left->val == right->val);
-    else if(strcmp(op, "<") == 0)
-        res->val = (float)(left->val < right->val);
-    else if(strcmp(op, "<=") == 0)
-        res->val = (float)(left->val <= right->val);
-    else if(strcmp(op, ">") == 0)
-        res->val = (float)(left->val > right->val);
-    else if(strcmp(op, ">=") == 0)
-        res->val = (float)(left->val >= right->val);
-    return res;
-}
-
-struct expr_type* expr_aritm(struct expr_type *a, struct expr_type *b, char op){
-    struct expr_type* res = (struct expr_type*) malloc(sizeof(struct expr_type));
-    res->tip = max(a->tip, b->tip);
-    switch(op){
-        case '+':
-            res->val = a->val + b->val;
-            break;
-        case '-':
-            res->val = a->val - b->val;
-            break;
-        case '*':
-            res->val = a->val * b->val;
-            break;
-        case '/':
-            res->val = a->val / b->val;
-    }
-    return res;
 }
